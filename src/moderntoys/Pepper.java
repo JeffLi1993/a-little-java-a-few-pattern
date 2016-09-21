d2729d441bbdbd745c6c56874bef8e6c9eb0af90
liqiangqiang <liqiangqiang@youzan.com>
2016-09-21 20:14:31 +0800

    1. Modern Toys

---
+++ src/moderntoys/Pepper.java
@@ -0,0 +1,7 @@
+package moderntoys;
+
+/**
+ * Created by bysocket on 16/9/21.
+ */
+public class Pepper extends Seasoning {
+}
---
+++ src/moderntoys/Seasoning.java
@@ -0,0 +1,7 @@
+package moderntoys;
+
+/**
+ * Created by bysocket on 16/9/21.
+ */
+public abstract class Seasoning {
+}
