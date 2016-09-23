b626fd17a276b5f6a2c65fc9feac35bf1947cc05
JeffLi1993 <qiangqiangli1993@gmail.com>
2016-09-24 00:45:56 +0800

    1. Modern Toys

---
+++ src/moderntoys/Num.java
@@ -0,0 +1,7 @@
+package moderntoys;
+
+/**
+ * Created by bysocket on 16/9/23.
+ */
+public abstract class Num {
+}
---
+++ src/moderntoys/OneMoreThan.java
@@ -0,0 +1,13 @@
+package moderntoys;
+
+/**
+ * Created by bysocket on 16/9/23.
+ */
+public class OneMoreThan extends Num {
+
+    Num predecessor;
+
+    OneMoreThan(Num predecessor) {
+        this.predecessor = predecessor;
+    }
+}
